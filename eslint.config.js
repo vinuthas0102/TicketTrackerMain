8ac2b93683b62395f622bacff3ae50bafcb8d50e
vinuthas0102 <vinuthas0102@gmail.com>
2025-11-22 12:40:47 +0530

    Updated eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,12 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-useless-catch': 'warn',
+      'no-unsafe-optional-chaining': 'warn',
+      'no-case-declarations': 'warn',
+      'no-useless-escape': 'warn',
     },
   }
 );
